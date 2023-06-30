230bb93aad2a4af8a56b498fcb79af04e4d2aa18
Choogi <drewtex@gmail.com>
2023-06-30 13:00:40 -0500

    routes all fixed

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const WeaponRoutes = require('./weapon-routes');
 const ClassRoutes = require('./class-routes');
 const ArmorRoutes = require('./armor-routes');
+const RingRoutes = require('./ring-routes');
 
 
-router.use('/categories', WeaponRoutes);
-router.use('/classes', ClassRoutes);
+router.use('/weapon', WeaponRoutes);
+router.use('/class', ClassRoutes);
 router.use('/armor', ArmorRoutes);
+router.use('/ring', RingRoutes);
 
 
 module.exports = router;
